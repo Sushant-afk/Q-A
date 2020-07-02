7a0184f5760054f75932d932de371c4266d815e0
sushant yadav <56043218+Sushant-afk@users.noreply.github.com>
2020-07-02 09:48:27 +0530

    Add files via upload

---
+++ models/profile.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const User = require('./user');
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    status: {
+        type: String
+    },
+    bio: {
+        type: String
+    },
+    knowsAbout:{
+        type: [String]
+    },
+    location: {
+        type: String
+    },
+    education: {
+        type: String
+    },
+    notifications: {
+        type: [String]
+    }
+
+});
+
+const Profile = mongoose.model('Profile', ProfileSchema);
+
+exports.Profile = Profile;
